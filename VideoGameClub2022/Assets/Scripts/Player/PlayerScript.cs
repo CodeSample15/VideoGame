c1c430aeec81c29336e05e443bc6ea903200c5b4
SmiggyBalls96 <themagicpony2@gmail.com>
2022-04-04 17:37:34 -0400

    Shadergraph / Improved Highlights
    To do: clean code, use material arrays instead of replacing the material

--- VideoGameClub2022/Assets/Scripts/Player/PlayerScript.cs
+++ VideoGameClub2022/Assets/Scripts/Player/PlayerScript.cs
@@ -104,19 +104,25 @@ public class PlayerScript : MonoBehaviour
 
     private bool Key;
     private GameObject hitObject;
+    [SerializeField]
+    private Material originalMaterial;
+    [SerializeField]
+    private Material highlightShader;
     void objectInteraction()        //Point of this function is to put a highlight around object that you can interact with, and allow interactions
     {
 
         RaycastHit interact;
         Physics.Raycast(camera.transform.position, camera.transform.forward, out interact, 3);    //3 is how far the player can interact with objects
-        
 
         if (Physics.Raycast(camera.transform.position, camera.transform.forward, out interact, 3))  // if Raycast hits something
         {
             if (interact.collider.tag == "Interaction") //If the object is tagged as an interaction object
             {
-                hitObject = interact.collider.gameObject;    
-                hitObject.GetComponent<MeshRenderer>().material.color = Color.white;    //Sets the color of object to white, (later change this to a highlight around object)
+                /*
+                 * Implement later, instead of replacing the material, instead add another element in the material array
+                 */
+                hitObject = interact.collider.gameObject;
+                hitObject.GetComponent<MeshRenderer>().material = highlightShader;    //Sets the color of object to white, (later change this to a highlight around object)
                 Key = true;
             }
             else //Any other object
@@ -132,7 +138,7 @@ public class PlayerScript : MonoBehaviour
         }
         else if (Key == true)   //If not hitting anything, return to normal color
         {
-            hitObject.GetComponent<MeshRenderer>().material.color = Color.grey; //Return to original color (with the highlights, just disable the highlight)
+            hitObject.GetComponent<MeshRenderer>().material = originalMaterial; //Return to original color (with the highlights, just disable the highlight)
             Key = false;
         }
 
